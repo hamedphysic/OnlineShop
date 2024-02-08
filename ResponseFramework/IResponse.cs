2c0f72ce3eecc312a9d36d9ab911320488e3c6cb
hamedphysic <hamed.physic@gmail.com>
2024-02-08 17:56:18 +0330

    response

---
+++ ResponseFramework/IResponse.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Net;
+
+namespace ResponseFramework
+{
+    internal interface IResponse<TResult>
+    {
+        bool IsSuccess { get; set; }
+        string? Message { get; set; }
+        string? ErrorMessage { get; set; }
+        TResult? Result { get; set; }
+        HttpStatusCode HttpStatusCode { get; set; }
+    }
+}
