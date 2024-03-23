14399604535ddd3db1f07e9b54b2e367fef665d1
hamedphysic <hamed.physic@gmail.com>
2024-03-23 17:57:22 +0330

    service rc

---
+++ OnlineShopApplication/Services/Contracts/IProductService.cs
@@ -0,0 +1,19 @@
+﻿using OnlineShopApplication.Dtos.SaleAppDto.ProductAppDto;
+using ResponseFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShopApplication.Services.Contracts
+{
+    public interface IProductService
+    {
+        Task<IResponse<object>> Post(PostProductAppDto model);
+        Task<IResponse<object>> Put(PutProductAppDto model);
+        Task<IResponse<object>> Delete(DeleteProductAppDto model);
+        Task<IResponse<List<GetProductAppDto>>> GetAll();
+
+    }
+}
---
+++ OnlineShopApplication/Services/SaleServices/ProductService.cs
@@ -0,0 +1,40 @@
+﻿using OnlineShopApplication.Dtos.SaleAppDto.ProductAppDto;
+using OnlineShopApplication.Services.Contracts;
+using ResponseFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace OnlineShopApplication.Services.SaleServices
+{
+    public class ProductService : IProductService
+   {
+    //    private readonly Onlineshope.RepositoryDesignPttern.Frameworks.Abstracts.IRepository _Irepository;
+        public ProductService()
+        {
+        }
+
+        public Task<IResponse<object>> Delete(DeleteProductAppDto model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IResponse<List<GetProductAppDto>>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IResponse<object>> Post(PostProductAppDto model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IResponse<object>> Put(PutProductAppDto model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
